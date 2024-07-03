45f3d3628ebeeddf6df629f17d68f67c26ab8088
JOTHIKA <rjothika2255@gmail.com>
2024-07-03 16:05:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let img = document.querySelector('.imgDisplay')
+let display_img = document.createElement('img')
+
+get_img()
+
+function get_img() {
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            let fetchData = fetch('https://api.slingacademy.com/v1/sample-data/photos')
+            fetchData.then((res) => {
+                return res.json()
+            }).then((data) => {
+                let x = Math.floor(Math.random() * 10)
+
+                let final = data.photos[x].url
+
+                display_img.setAttribute('src', `${final}`)
+                img.appendChild(display_img)
+                display_img.className ="headerImg"
+
+            })
+            resolve()
+
+        }, 1500)
+
+    })
+
+}
+let fetch_img = fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20')
+fetch_img.then((response) => {
+    return response.json()
+}).then((imgs) => {
+    // console.log(imgs)
+    let a = imgs.photos
+    console.log(a)
+
+    a.map((item) => {
+        let gallery_section = document.querySelector('.gallery_section')
+       
+            const imgg = document.createElement('img');
+            imgg.className = "allImg"
+            imgg.src= item.url
+            gallery_section.appendChild(imgg);
+        
+    })
+
+})
+
+// https://www.slingacademy.com/article/sample-photos-free-fake-rest-api-for-practice/
+// https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20
